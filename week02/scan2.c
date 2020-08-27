e3abb1cf52f8c3fabd16740c4864bc09438513a1
Phil Peterson <phpeterson@usfca.edu>
2020-08-27 10:17:07 -0700

    scanners

--- week02/scan2.c
+++ week02/scan2.c
@@ -5,11 +5,12 @@
 #define SCAN_TOKEN_LEN 32
 
 enum scan_token_enum {
+    TK_INTEGER,
     TK_PLUS,
     TK_MINUS,
     TK_MUL,
     TK_DIV,
-    TK_EOT,    
+    TK_EOT,
 };
 
 struct scan_token_st {
@@ -34,10 +35,34 @@ void scan_table_add(struct scan_table_st *st, enum scan_token_enum id, char *n) 
     st->len += 1;
 }
 
+bool is_digit(char c) {
+/* return (c >= '0' && c <= '9') ? true : false */
+    if (c >= '0' && c <= '9') {
+        return true;
+    }
+    return false;
+}
+
+char *scan_integer(struct scan_table_st *st, char *p, char *end) {
+    char str[SCAN_TOKEN_LEN];
+    char *ps = str;
+    str[0] = '\0';
+    
+    while (is_digit(*p) && p != end) {
+        *ps = *p;
+        ps += 1;
+        p += 1;
+    }
+    scan_table_add(st, TK_INTEGER, str);
+    return p;
+}
+
 char *scan_table_scan(struct scan_table_st *st, char *p, char *end) {
     if (p == end) {
         scan_table_add(st, TK_EOT, "");
         return NULL;
+    } else if (is_digit(*p)) {
+        return scan_integer(st, p, end);
     } else if (*p == '+') {
         scan_table_add(st, TK_PLUS, "+");
     } else if (*p == '-') {
@@ -51,7 +76,7 @@ char *scan_table_scan(struct scan_table_st *st, char *p, char *end) {
 }
 
 void scan_table_print(struct scan_table_st *st) {
-    char *token_names[] = {"TK_PLUS", "TK_MINUS", "TK_MUL", "TK_DIV", "TK_EOT"};
+    char *token_names[] = {"TK_INTEGER", "TK_PLUS", "TK_MINUS", "TK_MUL", "TK_DIV", "TK_EOT"};
 
     for (int i = 0; i < st->len; i++) {
         struct scan_token_st *pt = &st->table[i];
@@ -63,7 +88,7 @@ int main(int argc, char **argv) {
     struct scan_table_st scan_tbl;
     scan_table_init(&scan_tbl);
 
-    char *p = "+-*/";
+    char *p = "+-*/123";
     char *end = &p[strlen(p)];
     do {
         p = scan_table_scan(&scan_tbl, p, end);
